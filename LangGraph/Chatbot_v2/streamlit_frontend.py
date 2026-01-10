58be292a12ff575ec6fb0ab88e9f1d41af8804c9
Vedaant <vedaantmukherjee14@gmail.com>
2026-01-10 22:43:50 +0530

    Added Sidebar and loaded Previous Conversation

--- LangGraph/Chatbot_v2/streamlit_frontend.py
+++ LangGraph/Chatbot_v2/streamlit_frontend.py
@@ -1,20 +1,84 @@
+from numpy import add
 import streamlit as st
 from langgraph_backend import workflow
 from langchain_core.messages import HumanMessage, AIMessage
+import uuid
+
+########################### Utility Functions ##########################
+
+def generate_thread_id():
+    thread_id = uuid.uuid4()
+    return thread_id
+
+def reset_chat():
+    thread_id = generate_thread_id()
+    st.session_state['thread_id'] = thread_id
+    add_threads(thread_id)
+    st.session_state['message_history'] = []
+
+def add_threads(thread_id):
+    if thread_id not in st.session_state['chat_threads']:
+        st.session_state['chat_threads'].append(thread_id)
+
+def load_coversation(thread_id):
+    state = workflow.get_state(config={"configurable": {"thread_id": thread_id}})
+    return state.values.get('response', [])
 
 
-CONFIG = {"configurable": {"thread_id": "thread_1"}}
+############################ Session Setup#######################
 
 if 'message_history' not in st.session_state:
     st.session_state['message_history'] = []
 
+if 'thread_id' not in st.session_state:
+    st.session_state['thread_id'] = generate_thread_id()
+
+if 'chat_threads' not in st.session_state:
+    st.session_state['chat_threads'] = []
+
+add_threads(st.session_state['thread_id'])
+
+
+ ######################### Sidebar UI ##########################
+
+st.sidebar.title("LangGraph Chatbot")
+
+if st.sidebar.button('New Chat'):
+    reset_chat()
+
+
+st.sidebar.header('My Conversations')
+
+for thread_id in st.session_state['chat_threads']:
+    if st.sidebar.button(str(thread_id)):
+        st.session_state['thread_id'] = thread_id
+        messages = load_coversation(thread_id)
+
+
+        temp_messages = []
+
+        for msg in messages:
+            if isinstance(msg, HumanMessage):
+                role = 'user'
+            elif isinstance(msg, AIMessage):
+                role = 'assistant'
+            temp_messages.append({'role': role, 'content': msg.content})
+
+        st.session_state['message_history'] = temp_messages
+
+
+########################### Main UI ##########################
 
 for message in st.session_state['message_history']:
    with st.chat_message(message['role']):
        st.text(message['content'])
 
 
+
+
 user_input = st.chat_input("Type here")
+
+CONFIG = {"configurable": {"thread_id": st.session_state['thread_id']}}
 
 if user_input:
 
@@ -29,7 +93,7 @@ if user_input:
         ai_message = st.write_stream(
             message_chunk.content for message_chunk, metadata in workflow.stream(
                 {'response': [HumanMessage(content=user_input)]},
-                config= {'configurable': {'thread_id': 'thread-1'}},
+                config= CONFIG,
                 stream_mode= 'messages'
             )
         )
